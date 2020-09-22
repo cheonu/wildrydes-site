f731da972021b0637d6e47deac66c76d199a9608
Bamiche Onu <66482565+cheonu@users.noreply.github.com>
2020-09-22 16:39:42 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_v5kJzFBOt', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '14ckoqqte2p0kpie3jgusdvsd4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: 'https://5mzx8nqm4f.execute-api.us-east-1.amazonaws.com/prod',
